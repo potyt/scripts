e8b52ca7960a6ecf84219170ab915fbbcd3788ad
pot <pot@allus.ph>
2013-06-07 03:27:05 +0100

    Added support for a biff.txt short name.
    A biff.txt file will be used, if present, to determine the name for the
    account, rather than the name of the directory.

--- biff.sh
+++ biff.sh
@@ -1,7 +1,13 @@
 #! /bin/sh
 
 for dir in ~/mail/*; do
-    printf "`basename $dir`: "
+    biff="$dir/biff.txt"
+    if [ -r "$biff" -a -f "$biff" ]; then
+        biffname=`cat $biff`
+    else
+        biffname=`basename $dir`
+    fi
+    printf " $biffname: "
     chk4mail -c -d "$dir" -e "Trash" -l \
         | sed -e "s/^.* \([-0-9]\+\) \+new.*$/\1/g" \
         | sed -e "s/-/0/g" \
