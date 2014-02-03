2f8ebe41c1ff4ca2f5cc5cdc2ace2c31c890072a
pot <p@pot.yt>
2014-02-03 15:55:56 +0000

    Updated so argument is not required.

--- signature.sh
+++ signature.sh
@@ -4,5 +4,7 @@ script=`basename $0`
 fortune=$1
 
 base=${script%.*}
-fortune ~/fortune/$fortune
-printf "  // %s\n" `cat ~/.signature/$base`
+[[ -x "$fortune" ]] && fortune ~/fortune/$fortune || fortune
+if [ -e ~/.signature/$base ]; then
+    printf "  // %s\n" `cat ~/.signature/$base`
+fi
