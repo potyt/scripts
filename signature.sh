88b10dbadfc726c9209ada4b66d4bdca996da714
pot <p@pot.yt>
2014-03-02 23:13:27 +0000

    Change location of fortune files.

--- signature.sh
+++ signature.sh
@@ -4,7 +4,7 @@ script=`basename $0`
 fortune=$1
 
 base=${script%.*}
-[[ -x "$fortune" ]] && fortune ~/fortune/$fortune || fortune
+[[ -x "$fortune" ]] && fortune ~/.fortune/$fortune || fortune
 if [ -e ~/.signature/$base ]; then
     printf "  // %s\n" `cat ~/.signature/$base`
 fi
