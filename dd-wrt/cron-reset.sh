e1580ab58f581281e9c6f86c372c8772dd200779
pot <p@pot.yt>
2017-09-16 23:48:13 +0100

    Updates.

--- dd-wrt/cron-reset.sh
+++ dd-wrt/cron-reset.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+PATH=/jffs/scripts:$PATH
+
+ntp-sync.sh
+vpn-startall.sh
+
 while true; do
+    ntp-sync.sh
     log.sh "Restarting cron service"
     stopservice cron && startservice cron
-    sleep 3600
+    sleep 600
 done
--- dd-wrt/ntp-sync.sh
+++ dd-wrt/ntp-sync.sh
@@ -6,8 +6,11 @@ log.sh "# NTP sync $(date)"
 
 Ip=$(ip-cacher.sh $(nvram get ntp_server))
 if [[ -n $Ip ]]; then
-    log.sh "Syncing to NTP server $Ip"
+    log.sh "# Syncing to NTP server $Ip"
     firewall-hole.sh $Ip I
     ntpclient $Ip && stopservice process_monitor && startservice process_monitor
+    sleep 5
     firewall-hole.sh $Ip D
+else
+    log.sh "# Not syncing to NTP server $Ip"
 fi
