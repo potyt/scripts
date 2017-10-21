22ea705d5382b278d8d2494d5309f46cd7609efe
pot <p@pot.yt>
2017-10-22 00:54:47 +0100

    Updated to handle new pids.

--- dd-wrt/vpn-stop.sh
+++ dd-wrt/vpn-stop.sh
@@ -4,20 +4,27 @@ PATH=/jffs/scripts:$PATH
 
 idx=$1
 
+pid=""
 pidfile=/var/tmp/openvpn-$idx.pid
 if [[ -r $pidfile ]]; then
     pid=$(cat $pidfile)
     rm -f $pidfile
-    if [[ -n $dns ]]; then
-        log.sh "Killing pid $pid"
-        kill $pid
+else
+    log.sh "Can't find pidfile $pidfile"
+    pid=$(ps | grep openvpn | grep client-$idx | sed "s/^[ \t]*//" | cut -d" " -f1)
+fi
+
+if [[ -n $dns ]]; then
+    log.sh "Killing pid $pid"
+    kill $pid
+    if [[ $? = 0 ]]; then
         sleep 10
         kill -9 $pid
     else
-        log.sh "Can't find pid to kill"
+        log.sh "Can't find process $pid to kill"
     fi
 else
-    log.sh "Can't find pidfile $pidfile"
+    log.sh "Can't find pid to kill"
 fi
 
 conf=/jffs/etc/openvpn/client-$idx.conf
