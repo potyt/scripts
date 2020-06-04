9f05b6b3a8fd9af354b30b3f63261a8756988404
pot <p@pot.yt>
2020-06-04 13:04:24 +0100

    Updated.

---
+++ synology-fix-download-perms.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -e
+
+chmod -R a+rwx /volume1/downloads/nzbget/completed
+chmod o-w /volume1/downloads/nzbget/completed
--- synology-fix-media-perms.sh
+++ synology-fix-media-perms.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-find /volume1/downloads/nzbget/completed -type f -exec chmod a+rw {} \;
-find /volume1/downloads/nzbget/completed -mindepth 1 -type d -exec chmod a+rwx {} \;
-
 chown -R sc-headphones.headphones /volume1/musik/incoming
 chmod -R ug+rw /volume1/musik/incoming
 chmod -R o-w /volume1/musik/incoming
